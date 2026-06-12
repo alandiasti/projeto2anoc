c7133af918c0b0ad5ed22000217d83d5a869511b
Alan Dias <alandias.silva08@gmail.com>
2026-06-12 10:06:43 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const alunos = [
+    {nome:"Ayrla", foto:"/img/Ayrla.webp"},
+    {nome:"José Francisco", foto:"/img/jose.jpeg"},
+    {nome:"Kaio", foto:"/img/Kaio.webp"},
+    {nome:"Luanna", foto:"/img/luanna.jpeg"},
+    {nome:"Maria Lúcia", foto:"/img/Maria_L.webp"},
+    {nome:"Maria Clara", foto:"/img/MariaC.jpg"},
+    {nome:"Maria Rita", foto:"/img/MariaR.jpeg"},
+    {nome:"Lorena", foto:"/img/lorena.jpeg"},
+    {nome:"Rebeca", foto:"/img/Rebeca.jpg"},
+    {nome:"Sara Maria", foto:"/img/sara.jpeg"},
+    {nome:"Taynara", foto:"/img/Taynara.jpg"},
+    {nome:"Victor Emanuel", foto:"/img/Victor.jpeg"},
+    {nome:"Davi", foto:"/img/Davi.png"},
+    {nome:"Fabiola", foto:"/img/Fabiola.jpeg"},
+    {nome:"Francisco Vitor", foto:"/img/FranciscoV.jpeg"},
+    {nome:"Geovana", foto:"/img/Geovanna.jpeg"},
+    {nome:"João Pedro", foto:"/img/JoaoP.jpeg"},
+    {nome:"Kauane", foto:"/img/Kauane.jpeg"},
+    {nome:"Isabelly", foto:"/img/isabely.jpeg"},
+    {nome:"Isis", foto:"/img/isis.jpeg"},
+    {nome:"João Victor", foto:"/img/JoaoV.jpeg"},
+    {nome:"Luis", foto:"/img/Luis.jpg"},
+    {nome:"Manoel Vitor", foto:"/img/ManoelV.jpeg"},
+    {nome:"Michael", foto:"/img/Michael.jpeg"},
+    {nome:"Mikaelly", foto:"/img/Mikaely.jpeg"},
+
+];
+
+const galeria = document.getElementById("galeria");
+
+alunos.forEach(aluno=>{
+    galeria.innerHTML += `
+        <div class="card">
+            <img src="${aluno.foto}" alt="${aluno.nome}">
+            <p>${aluno.nome}</p>
+        </div>
+    `;
+});
